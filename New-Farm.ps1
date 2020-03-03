9739e4d78ab121e101c9a02b5775d6ab7492c0c5
AMadianov <40032430+AMadianov@users.noreply.github.com>
2020-03-03 16:37:30 +0300

    next

---
+++ New-Farm.ps1
@@ -0,0 +1,3 @@
+﻿#Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+New-SPConfigurationDatabase -DatabaseName "SPS_Config" -DatabaseServer SPSSQL -AdministrationContentDatabaseName "SPS_Admin_Content" -Passphrase (ConvertTo-SecureString "fuckMAN!pass" -AsPlainText -Force) -FarmCredentials (Get-Credential) -SkipRegisterAsDistributedCacheHost
