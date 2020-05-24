838004b25a9620b14808639eb86c83e687ce0e7f
Alex Madianov <a.madianov@gmail.com>
2020-05-24 17:07:08 +0300

    del old files

--- new-state_service.ps1
+++
@@ -1,8 +0,0 @@
-#Add-PSSnapin Microsoft.SharePoint.PowerShell
-
-## Settings you may want to change ##
-$DatabaseServerName = "SPSSQL"
-$DatabaseName = "State_Service"
-$stateSAName = "State Service"
-
-New-SPStateServiceDatabase -Name $DatabaseName -DatabaseServer $DatabaseServerName | New-SPStateServiceApplication -Name $stateSAName | New-SPStateServiceApplicationProxy -Name ($stateSAName+" Proxy") -DefaultProxyGroup > $null
